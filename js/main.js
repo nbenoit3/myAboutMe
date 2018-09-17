cd2095f495dacbae06212f0fbcb4ac0d44904045
Nick Benoit <nbenoit3@gmail.com>
2018-09-17 18:44:55 -0400

    changed the index.html

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('#sidebar-btn').click(function(){
+      $('#sidebar').toggleClass('visible');
+    })
+  });
+
+  $(document).ready(function(){
+    $('#funko').click(function(){
+      $('#hello').toggleClass('switch');
+    })
+  });
